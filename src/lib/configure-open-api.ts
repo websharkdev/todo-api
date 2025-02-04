5d4b969b3de2dc788c0843cf907d55131a9976f2
Oleksii <alexey.bortnytskyi@gmail.com>
2025-02-04 13:17:22 +0100

    refactor(lib/configure-open-api) - set <version> for <app.doc> from 'package.json'

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,11 +1,11 @@
+import packageJSON from '../../package.json';
 import { AppOpenAPI } from "./types";
-import packageJSON from '../../package.json'
 
 const configureOpenAPI = (app: AppOpenAPI) => {
     app.doc('/doc', {
         openapi: "3.0.0",
         info: {
-            version: '1.0.0',
+            version: packageJSON.version,
             title: "Todo API"
         }
     })
